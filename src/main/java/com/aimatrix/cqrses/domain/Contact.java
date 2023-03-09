06bcf3d15dbd8410cab668848f8e24dea937c79f
luuthuanduc <ducluuthuan@gmail.com>
2023-03-09 14:03:54 +0700

    1. CRUD

---
+++ src/main/java/com/aimatrix/cqrses/domain/Contact.java
@@ -0,0 +1,13 @@
+package com.aimatrix.cqrses.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Contact {
+
+    private String type;
+    private String detail;
+
+}
